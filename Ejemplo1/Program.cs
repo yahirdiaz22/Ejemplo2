3e7a9063420e048a0f53cfb16b71c1643921bea9
ERIK YAHIR DIAZ MARTINEZ <I20050014@monclova.tecnm.mx>
2020-09-09 17:12:16 -0500

    vas bien

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola a todos, ejemplo!");
+        }
+    }
+}
